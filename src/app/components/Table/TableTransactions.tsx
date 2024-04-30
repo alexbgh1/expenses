538e6080bdf5052aefdd3b43fabaf9ec90ae21e8
alexbgh1 <alex.garnica@alumnos.uach.cl>
2024-04-29 20:04:19 -0400

    Table empty Button to do some actions

--- src/app/components/Table/TableTransactions.tsx
+++ src/app/components/Table/TableTransactions.tsx
@@ -1,12 +1,16 @@
+import { MoreVerticalIcon } from "@/app/icons/icons";
 const TableTransactions = () => {
   return (
-    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-      <thead className="text-xs font-light text-gray-500 hover: [&_tr]:border-b hover:bg-gray-50">
+    <table className="w-full text-sm text-left rtl:text-right text-gray-500">
+      <thead className="text-xs font-light hover:bg-gray-50">
         <tr className="border-b transition-colors">
           <th className="h-12 px-4 py-2">Date</th>
           <th className="h-12 px-4 py-2">Description</th>
           <th className="h-12 px-4 py-2">Category</th>
-          <th className="h-12 px-4 py-2">Amount</th>
+          <th className="text-right h-12 px-4 py-2">Amount</th>
+          <th className="h-12 px-4 py-2">
+            <span className="sr-only">Actions</span>
+          </th>
         </tr>
       </thead>
       <tbody className="[&_tr:last-child]:border-0">
@@ -14,19 +18,37 @@ const TableTransactions = () => {
           <td className="px-4 py-2">2021-01-01</td>
           <td className="px-4 py-2">Rent</td>
           <td className="px-4 py-2">Housing</td>
-          <td className="px-4 py-2">$1000</td>
+          <td className="text-right px-4 py-2">$1000</td>
+          <td className="px-4 py-2">
+            <button className="hover:text-gray-800 hover:bg-gray-200 py-2 px-1">
+              <MoreVerticalIcon className="w-4 h-4" />
+              <span className="sr-only">Edit</span>
+            </button>
+          </td>
         </tr>
         <tr className="text-gray-700 hover:bg-gray-50 border-b transition-colors">
           <td className="px-4 py-2">2021-01-02</td>
           <td className="px-4 py-2">Groceries</td>
           <td className="px-4 py-2">Groceries</td>
-          <td className="px-4 py-2">$100</td>
+          <td className="text-right px-4 py-2">$100</td>
+          <td className="px-4 py-2">
+            <button className="hover:text-gray-800 hover:bg-gray-200 py-2 px-1">
+              <MoreVerticalIcon className="w-4 h-4" />
+              <span className="sr-only">Edit</span>
+            </button>
+          </td>
         </tr>
         <tr className="text-gray-700 hover:bg-gray-50 border-b transition-colors">
           <td className="px-4 py-2">2021-01-03</td>
           <td className="px-4 py-2">Gas</td>
           <td className="px-4 py-2">Transportation</td>
-          <td className="px-4 py-2">$50</td>
+          <td className="text-right  px-4 py-2">$50</td>
+          <td className="px-4 py-2">
+            <button className="hover:text-gray-800 hover:bg-gray-200 py-2 px-1">
+              <MoreVerticalIcon className="w-4 h-4" />
+              <span className="sr-only">Edit</span>
+            </button>
+          </td>
         </tr>
       </tbody>
     </table>
